68e33a97768619ed64155653e3fe866ad3f43820
FelixCrisan <crisanfelix@yahoo.com>
2021-11-15 23:35:07 +0200

    Add project files.

---
+++ Data/Crisan_FelixMarian_Lab8Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Crisan_FelixMarian_Lab8.Models;
+
+namespace Crisan_FelixMarian_Lab8.Data
+{
+    public class Crisan_FelixMarian_Lab8Context : DbContext
+    {
+        public Crisan_FelixMarian_Lab8Context (DbContextOptions<Crisan_FelixMarian_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Crisan_FelixMarian_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Crisan_FelixMarian_Lab8.Models.Publisher> Publisher { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Crisan_FelixMarian_Lab8.Data;
+using Crisan_FelixMarian_Lab8.Models;
+
+namespace Crisan_FelixMarian_Lab8.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Crisan_FelixMarian_Lab8.Data.Crisan_FelixMarian_Lab8Context _context;
+
+        public IndexModel(Crisan_FelixMarian_Lab8.Data.Crisan_FelixMarian_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book
+                .Include(b => b.Publisher)
+                .ToListAsync();
+        }
+    }
+}
